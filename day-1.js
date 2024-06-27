41ffe3ea701fcba8281c9d1a742f1df4e63be43b
Adia Holt <adia.holt@gmail.com>
2024-06-26 22:19:11 -0500

    update day 1

--- day-1.js
+++ day-1.js
@@ -12,10 +12,12 @@ function day1Part1() {
   // Try it!
   // TODO: add a new question for the player. Assign the answer to the playerOne Object.
 
+  playerOne.age = prompt ("What is your age?");
+  playerOne.sign = prompt("What is your sign?");
 
   // Try it!
   // TODO: add the answer to the new question here.
-  messageParagraph.innerHTML = `Hi, ${playerOne.name}! Can you beat your hi score of ${playerOne.hiScore}?`;
+  messageParagraph.innerHTML = `Hi, ${playerOne.name}! Your astrological sign is ${playerOne.sign}. Can you beat your hi score of ${playerOne.hiScore}?`;
 
 }
 
@@ -23,17 +25,18 @@ part1.onclick = day1Part1;
 
 
 function day1Part2() {
-  let customer = {};
-  customer.name = prompt("What is your name?");
-  customer.address = prompt("What is your address?");
+  let playerTwo = {};
+  playerTwo.name = prompt("What is your name?");
+  playerTwo.address = prompt("What is your address?");
   // Try it!
   // TODO: add a new question for the player. Assign the answer to the playerOne Object.
   
+  playerTwo.phoneNumber = prompt("What is your phone number");
 
   
   // Try it!
   // TODO: add the answer to the new question to the innerHTML statment below.
-  messageParagraph.innerHTML = `Hi, ${customer.name}! Your current address is ${customer.address}.`;
+  messageParagraph.innerHTML = `Hi, ${playerTwo.name}! Your current address is ${playerTwo.address}. Your current phone number is ${playerTwo.phoneNumber}.`;
 }
 part2.onclick = day1Part2;
 
@@ -42,12 +45,12 @@ function day1Part3() {
   let pet;
   pet = {};
 
-  // Try it!
   // TODO: create a prompt to ask about a pet
-
+  
+  pet.name = prompt ("What is your pet's name?");
   // Try it!
   // TODO: make a message based on the answers to the prompt.
-  messageParagraph.innerHTML = ``;
+  messageParagraph.innerHTML = `Your pets name is ${pet.name}.`;
 }
 part3.onclick = day1Part3;
 
@@ -55,7 +58,19 @@ part3.onclick = day1Part3;
 // Try it!
 // TODO: add your own idea for a prompt. Use an object to store the user's response.
 function day1Part4() {
-  alert("hello!");
+  let playerThree;
+  playerThree = {};
+
+  playerThree.name = prompt ("What is your name?");
+
+  let childOne;
+  childOne = {};
+  childOne.name = prompt ("What is your child's name?");
+
+  messageParagraph.innerHTML = `Hi, ${playerThree.name}. Your childs name is ${childOne.name}.`
+
+
+
 
   
 }
